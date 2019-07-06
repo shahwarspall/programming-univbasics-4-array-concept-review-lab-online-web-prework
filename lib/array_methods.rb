309030f6b671ce8fc87119f2dc0f1e824f287941
Shahwar Spall <shevy.spall@gmail.com>
2019-07-06 19:29:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,10 @@
 def find_element_index(array, value_to_find)
-
+ return array.index(value_to_find) {|index| return index}
 end
 
 
 def find_max_value(array)
-   array
+   return array.max
 end
 
 def find_min_value(array)
